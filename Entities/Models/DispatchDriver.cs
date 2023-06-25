adb328a7ecf66d42831784cfe6518c9f29f03139
Daniel Adjei <dadjei@seekapor.com>
2023-06-25 02:40:09 +0000

    first commit

---
+++ Entities/Models/DispatchDriver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    [Column("DispatchDriverId")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Full Name is a required field.")]
+    public string FullName { get; set; }
+
+    [Required(ErrorMessage = "Phone number is required.")]
+    [RegularExpression(@"^\d{10}$", ErrorMessage = "Invalid phone number.")]
+    public string PhoneNumber { get; set; }
+}
---
+++ FoodDelivery/Program.cs
@@ -0,0 +1,54 @@
+using FoodDelivery.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+
+namespace FoodDelivery
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+            builder.Services.ConfigureCors();
+            builder.Services.ConfigureLoggerService();
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment()) 
+                app.UseDeveloperExceptionPage(); 
+            else 
+                app.UseHsts();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles(); 
+            app.UseForwardedHeaders(new ForwardedHeadersOptions 
+            {
+                ForwardedHeaders = ForwardedHeaders.All 
+            }); 
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
